e451ca378412441b1ffddf690b1023bb927a5757
VirajJagtap001 <jviraj293@gmail.com>
2025-04-29 12:33:03 +0530

    Removed relative paths

--- run.py
+++ run.py
@@ -13,11 +13,11 @@ import pandas as pd
 import torch
 from easydict import EasyDict as edict
 
-from .config import get_config_regression
-from .data_loader import MMDataLoader
-from .models import AMIO
-from .trains import ATIO
-from .utils import assign_gpu, count_parameters, setup_seed
+from config import get_config_regression
+from data_loader import MMDataLoader
+from models import AMIO
+from trains import ATIO
+from utils import assign_gpu, count_parameters, setup_seed
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:2" # This is crucial for reproducibility
