6e9ed9b40d1369f05070ea6b45de569ec07456e1
VirajJagtap001 <jviraj293@gmail.com>
2025-04-30 11:50:20 +0530

    Print position ids

--- models/CENET.py
+++ models/CENET.py
@@ -463,11 +463,9 @@ class CENET(BertPreTrainedModel):
     def forward(self,text, acoustic, visual, visual_ids=None, acoustic_ids=None, pos_tag_ids=None, senti_word_ids=None, polarity_ids= None, position_ids=None, head_mask= None, labels=None):
         input_ids = text[:,0,:].long()
         input_ids = torch.clamp(input_ids, min=0)
-        print("Input IDs:", input_ids)
-        print("Min value in input_ids:", input_ids.min().item())
-        print("Max value in input_ids:", input_ids.max().item())
         attention_mask =text[:,1,:].long()
         token_type_ids = text[:,2,:].long()
+        Print("Position IDs:", position_ids)
         outputs = self.bert(input_ids=input_ids,
                             visual=visual,
                             acoustic=acoustic,
